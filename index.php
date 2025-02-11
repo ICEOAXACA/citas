02eb188a6b84c13c960ff822893c5e4bae411966
UnidadTecnica <iceoaxaca@gmail.com>
2025-02-11 15:58:00 -0600

    se agrego la ventana principal y el login sin funcion

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registro de Citas ICEO</title>
+  <link rel="icon" type="image/png" href="./Imagenes/favicon.ico">
+  </link> <!--linea de codigo para favicon-->
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <header>
+    <a href="https://www.oaxaca.gob.mx/iceo/" class="logo">
+      <img src="Imagenes/logo1.png" alt="logo" class="logo-img">
+    </a>
+
+    <nav>
+      <a href="login/login.html" class="nav-link">Iniciar sesion</a>
+    </nav>
+
+  </header>
+
+  <div class="contenedor">  
+  <h3 class="subtitulo">Registro de Citas</h3>
+  </div>
+
+</body>
+
+</html>
